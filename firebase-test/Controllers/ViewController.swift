4662a2ae2b3587fcd71b41f712fe781180c7bdf3
Miguel Angel Tomairo <migueltomairo@multiplica.com>
2019-12-25 20:45:48 -0500

    initial commit

---
+++ firebase-test/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-test
+//
+//  Created by miguel tomairo on 12/25/19.
+//  Copyright © 2019 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
